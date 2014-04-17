0c7832e482a90988a3d8304598af0e32266c4c86
jcorner <tcanto88@yahoo.com.br>
2014-04-17 16:52:06 -0300

    Tentei apagar tudo q tinha acento

--- index.php
+++ index.php
@@ -1,15 +1,1 @@
 Mudança do Thiago.
-
-
-<<<<<<< HEAD
-teste
-
-<<<<<<< HEAD
-
-
-=======
->>>>>>> 42fec91... Comentario adicionado
-=======
-
->>>>>>> 5c8c86b... Merge branch 'master' of github.com:mantonioc/teste_mantonio
-// Comentario
